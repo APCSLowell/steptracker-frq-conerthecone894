ab6419549c571a678d81d9b481682f878bf5f186
conerthecone894 <143015307+conerthecone894@users.noreply.github.com>
2024-04-21 21:47:39 -0700

    Update StepTracker.java
    attempt 1. should be correct since it all worked as intended in processing

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,40 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int days, active;
+    private ArrayList<Integer> steps = new ArrayList<Integer>();
+
+    public StepTracker(int a){
+         active = a;
+    }
+
+    public void addDailySteps(int s){
+        steps.add(s);
+        days++;
+    }
+
+    public int activeDays(){
+        int aDays = 0;
+
+        if(steps.size() != 0){
+            for(int i = 0; i < steps.size(); i++){
+                if(steps.get(i) >= active)
+                    aDays++;
+            }
+        }
+
+        return aDays;
+    }
+
+    public double averageSteps(){
+        int s = 0;
+        int d = 0;
+
+        for(int i = 0; i < steps.size(); i++){
+            s += steps.get(i);
+            d++;
+        }
+
+        return s/d;
+    }
 } 
